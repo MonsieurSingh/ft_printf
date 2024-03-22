cfe237b30cb09a6edaf0bcede8e44f84af331ffc
Teghjyot Singh <teghjyot@icloud.com>
2024-03-23 03:01:40 +1030

    Initial Commit

---
+++ ft_printf/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ft_printf
+//
+//  Created by Devjyot Singh on 23/3/2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
